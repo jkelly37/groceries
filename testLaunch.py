854c2907c407ac76e4f1f59ad732b716f8ddb022
Jack-kelly-22 <jack22kelly22@gmail.com>
2020-03-24 13:36:06 -0700

    ddd

--- testLaunch.py
+++ testLaunch.py
@@ -1,21 +1,12 @@
-import time
 
 from selenium import webdriver
 driver = webdriver.Chrome("/Users/jackkelly/PycharmProjects/appiumTest/chromedriver")
-driver.implicitly_wait(1)
-driver.get("https://www.starbucks.com/account/signin?ReturnUrl=%2F")
-time.sleep(0.5)
-loginemailText =driver.find_element_by_xpath('//*[@id="username"]')
-loginPasswordText =driver.find_element_by_xpath('//*[@id="password"]')
-
-loginemailText.send_keys("jacklaxjk@gmail.com")
-time.sleep(0.5)
-loginPasswordText.send_keys("Lacrosse22!")
-
-login_button = driver.find_element_by_xpath('//*[@id="content"]/div[2]/div/div/div/div/div[1]/form/div[6]/div/span/div/button')
-login_button.click()
-time.sleep(5)
-driver.close()
+driver.get("https://www.kingsoopers.com/signin?redirectUrl=/")
+emailText= driver.find_element_by_xpath('//*[@id="SignIn-emailInput"]')
+emailText.send_keys("liam.propst@gmail.com")
+passText = driver.find_element_by_xpath('//*[@id="SignIn-passwordInput"]')
+passText.send_keys("Liam123321")
+signInButton= driver.find_element_by_xpath('//*[@id="SignIn-submitButton"]')
 
 #driver.get("https://www.starbucks.com/menu")
 #driver.get("https://www.starbucks.com/menu/product/2123074/iced?parent=%2Fdrinks%2Ficed-teas%2Ficed-green-teas")
