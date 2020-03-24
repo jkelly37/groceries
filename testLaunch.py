adfa52dc8c3dc77621a0affc40c5f76a81c9e092
Jack-kelly-22 <jack22kelly22@gmail.com>
2020-03-24 14:04:27 -0700

    ddd

--- testLaunch.py
+++ testLaunch.py
@@ -1,4 +1,4 @@
-
+import time
 from selenium import webdriver
 driver = webdriver.Chrome("/Users/jackkelly/PycharmProjects/appiumTest/chromedriver")
 driver.get("https://www.kingsoopers.com/signin?redirectUrl=/")
@@ -7,6 +7,16 @@ emailText.send_keys("liam.propst@gmail.com")
 passText = driver.find_element_by_xpath('//*[@id="SignIn-passwordInput"]')
 passText.send_keys("Liam123321")
 signInButton= driver.find_element_by_xpath('//*[@id="SignIn-submitButton"]')
+signInButton.click()
+time.sleep(5)
+#confirm_store_button=driver.find_element_by_xpath('//*[@id="root"]/div[2]/div[2]/div[3]/div/div/div[6]/button[1]')
+time.sleep(1)
+
+#time.sleep(10)
+#cart_button = driver.find_element_by_class_name("kds-Link kds-Link--inherit CartLink")
+#driver.get("https://www.kingsoopers.com/cart")
+#delivery_button=driver.find_element_by_xpath('//*[@id="content"]/div/div/div/div[1]/div[2]/div/div/div/div[1]/div[2]/div[1]/button')
+
 
 #driver.get("https://www.starbucks.com/menu")
 #driver.get("https://www.starbucks.com/menu/product/2123074/iced?parent=%2Fdrinks%2Ficed-teas%2Ficed-green-teas")
